7a0f2c6c77fd6a25049fe3692735c1f4d9962386
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2022-08-06 18:18:26 -0700

    update
    update

--- pages/index.js
+++ pages/index.js
@@ -122,7 +122,14 @@ export default function Index() {
         <title>MENJi NFT Drop</title>
         <meta name="description" content="MENJi NFT Site by Kodiak" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:image" content="https://i.imgur.com/p6qgDQt.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <a href="/home">
             <Canvas style={{ width: '100%', height: '100%' }}
